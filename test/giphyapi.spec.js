3d194c4ac4b61523ffada612b1843c5130d5b11b
r0neel <ronbhagwakar@gmail.com>
2022-02-04 12:23:26 +0000

    giphy.spec.js update

--- test/giphyapi.spec.js
+++ test/giphyapi.spec.js
@@ -1,6 +1,14 @@
 /**
  * @jest-environment jsdom
  */
+const { giphySearch } = require("../client/giphyapi.js");
 
-const { doc } = require("prettier");
-const api = require("../client/giphyapi.js");
+describe("giphySearch function", () => {
+  let giphyKey = "UTn30CTrQ5AweWYK7c50BaP6Fd28hUr3";
+  let keyword = "dog";
+
+  it("returns a string", () => {
+    let giflink = giphySearch(keyword);
+    expect(typeof giflink.toBe(string));
+  });
+});
